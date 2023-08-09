35ce1a2bdf7d3573c024f1bfb9eb0e205c7e4e76
Privacy Sandbox Team <no-reply@google.com>
2023-08-09 14:45:18 +0000

    Add inline WASM example for protobuf
    Bug: b/291739953
    Change-Id: I9174735a1d216d0cd154e0bf1b9ebe1caea499b3
    GitOrigin-RevId: 1f161b06a5f141241fdf30ea35892965b6bebc5a

---
+++ tools/inline_wasm/examples/protobuf/my_udf.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+function getKeyGroupOutputs(input, module) {
+  const keyGroupOutputs = [];
+  for (const keyGroup of input.keyGroups) {
+    const keyGroupOutput = {};
+    keyGroupOutput.tags = keyGroup.tags;
+
+    const getValuesResult = JSON.parse(getValues(keyGroup.keyList));
+    // getValuesResult returns "kvPairs" when successful and "code" on failure.
+    // Ignore failures and only add successful getValuesResult lookups to output.
+    if (getValuesResult.hasOwnProperty("kvPairs")) {
+      const kvPairs = getValuesResult.kvPairs;
+      const keyValuesOutput = {};
+      for (const key in kvPairs) {
+        if (kvPairs[key].hasOwnProperty("value")) {
+          keyValuesOutput[key] = {
+            "value": module.HelloClass.SayHello(kvPairs[key].value)
+          };
+        }
+      }
+      keyGroupOutput.keyValues = keyValuesOutput;
+      keyGroupOutputs.push(keyGroupOutput);
+    }
+  }
+  return keyGroupOutputs;
+}
+
+async function HandleRequest(input) {
+  logMessage("Handling request");
+  const module = await getModule();
+  logMessage("Done loading WASM Module");
+  const keyGroupOutputs = getKeyGroupOutputs(input, module);
+  return { keyGroupOutputs, udfOutputApiVersion: 1 };
+}

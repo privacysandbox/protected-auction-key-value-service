2f53b38dd473f6ba82ea6f531223a64ec403e7a5
Privacy Sandbox Team <no-reply@google.com>
2023-08-10 15:54:14 -0400

    feat: A delta based request generator that creates KV requests from delta files
    Bug: b/293623535
    Change-Id: I8919acf00dc69b1c4dd502c63edaaa1790cb71df
    GitOrigin-RevId: 21d950309d02a22eb4c3e88dab81762c72172e29

---
+++ tools/request_simulation/delta_based_request_generator.cc
@@ -0,0 +1,162 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tools/request_simulation/delta_based_request_generator.h"
+
+#include <utility>
+
+#include "absl/functional/bind_front.h"
+#include "public/data_loading/data_loading_generated.h"
+#include "public/data_loading/filename_utils.h"
+#include "public/data_loading/records_utils.h"
+#include "src/cpp/telemetry/tracing.h"
+
+using privacy_sandbox::server_common::MetricsRecorder;
+using privacy_sandbox::server_common::TraceWithStatusOr;
+
+namespace kv_server {
+namespace {
+// Holds an input stream pointing to a blob of Riegeli records.
+class BlobRecordStream : public RecordStream {
+ public:
+  explicit BlobRecordStream(std::unique_ptr<BlobReader> blob_reader)
+      : blob_reader_(std::move(blob_reader)) {}
+  std::istream& Stream() { return blob_reader_->Stream(); }
+
+ private:
+  std::unique_ptr<BlobReader> blob_reader_;
+};
+}  // namespace
+
+absl::Status DeltaBasedRequestGenerator::Start() {
+  LOG(INFO) << "Start monitor and load delta files";
+  absl::Status status = options_.delta_notifier.Start(
+      options_.change_notifier, {.bucket = options_.data_bucket}, "",
+      absl::bind_front(&DeltaBasedRequestGenerator::EnqueueNewFilesToProcess,
+                       this));
+  if (!status.ok()) {
+    return status;
+  }
+  return data_load_thread_manager_->Start([this]() { ProcessNewFiles(); });
+}
+absl::Status DeltaBasedRequestGenerator::Stop() {
+  {
+    absl::MutexLock l(&mu_);
+    stop_ = true;
+  }
+  LOG(INFO) << "Stopping loading new data from " << options_.data_bucket;
+  if (options_.delta_notifier.IsRunning()) {
+    if (const auto status = options_.delta_notifier.Stop(); !status.ok()) {
+      LOG(ERROR) << "Failed to stop notify: " << status;
+    }
+  }
+  LOG(INFO) << "Delta notifier stopped";
+  LOG(INFO) << "Stopping loading new data";
+  return data_load_thread_manager_->Stop();
+}
+bool DeltaBasedRequestGenerator::IsRunning() const {
+  return data_load_thread_manager_->IsRunning();
+}
+void DeltaBasedRequestGenerator::EnqueueNewFilesToProcess(
+    const std::string& basename) {
+  absl::MutexLock l(&mu_);
+  unprocessed_basenames_.push_front(basename);
+  LOG(INFO) << "queued " << basename << " for loading";
+}
+void DeltaBasedRequestGenerator::ProcessNewFiles() {
+  LOG(INFO) << "Thread for new file processing started";
+  absl::Condition has_new_event(
+      this, &DeltaBasedRequestGenerator::HasNewEventToProcess);
+  while (!data_load_thread_manager_->ShouldStop()) {
+    std::string basename;
+    {
+      absl::MutexLock l(&mu_, has_new_event);
+      if (stop_) {
+        LOG(INFO) << "Thread for new file processing stopped";
+        return;
+      }
+      basename = std::move(unprocessed_basenames_.back());
+      unprocessed_basenames_.pop_back();
+    }
+    LOG(INFO) << "Loading " << basename;
+    if (!IsDeltaFilename(basename)) {
+      LOG(WARNING) << "Received file with invalid name: " << basename;
+      continue;
+    }
+    RetryUntilOk(
+        [this, &basename] {
+          // TODO: distinguish status. Some can be retried while others
+          // are fatal.
+          return TraceCreateRequestsAndAddToMessageQueue(
+              {.bucket = options_.data_bucket, .key = basename});
+        },
+        "LoadNewFileAndCreateRequest", &metrics_recorder_);
+  }
+}
+absl::StatusOr<DataLoadingStats>
+DeltaBasedRequestGenerator::CreateRequestsAndAddToMessageQueue(
+    const BlobStorageClient::DataLocation& location) {
+  LOG(INFO) << "Loading " << location;
+  auto& blob_client = options_.blob_client;
+  auto record_reader =
+      options_.delta_stream_reader_factory.CreateConcurrentReader(
+          metrics_recorder_,
+          /*stream_factory=*/[&location, &blob_client]() {
+            return std::make_unique<BlobRecordStream>(
+                blob_client.GetBlobReader(location));
+          });
+  auto metadata = record_reader->GetKVFileMetadata();
+  if (!metadata.ok()) {
+    return metadata.status();
+  }
+  DataLoadingStats data_loading_stats;
+  const auto process_data_record_fn = [this, &data_loading_stats](
+                                          const DataRecord& data_record) {
+    if (data_record.record_type() == Record::KeyValueMutationRecord) {
+      const auto* record = data_record.record_as_KeyValueMutationRecord();
+      if (record->value_type() == Value::String) {
+        options_.message_queue.Push(
+            request_generation_fn_(record->key()->string_view()));
+        if (record->mutation_type() == KeyValueMutationType::Update) {
+          data_loading_stats.total_updated_records++;
+        } else if (record->mutation_type() == KeyValueMutationType::Delete) {
+          data_loading_stats.total_deleted_records++;
+        }
+        return absl::OkStatus();
+      }
+    }
+  };
+  auto status = record_reader->ReadStreamRecords(
+      [&process_data_record_fn](std::string_view raw) {
+        return DeserializeDataRecord(raw, process_data_record_fn);
+      });
+  if (!status.ok()) {
+    return status;
+  }
+  return data_loading_stats;
+}
+bool DeltaBasedRequestGenerator::HasNewEventToProcess() const {
+  return !unprocessed_basenames_.empty() || stop_;
+}
+absl::StatusOr<DataLoadingStats>
+DeltaBasedRequestGenerator::TraceCreateRequestsAndAddToMessageQueue(
+    BlobStorageClient::DataLocation location) {
+  return TraceWithStatusOr(
+      [location, this] { return CreateRequestsAndAddToMessageQueue(location); },
+      "CreateRequestsAndAddToMessageQueue",
+      {{"bucket", std::move(location.bucket)},
+       {"key", std::move(location.key)}});
+}
+
+}  // namespace kv_server
---
+++ tools/request_simulation/delta_based_request_generator_test.cc
@@ -0,0 +1,151 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tools/request_simulation/delta_based_request_generator.h"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/synchronization/notification.h"
+#include "components/data/common/mocks.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "public/constants.h"
+#include "public/data_loading/filename_utils.h"
+#include "public/data_loading/records_utils.h"
+#include "public/test_util/mocks.h"
+#include "src/cpp/telemetry/mocks.h"
+#include "tools/request_simulation/request_generation_util.h"
+
+using kv_server::BlobStorageChangeNotifier;
+using kv_server::BlobStorageClient;
+using kv_server::DataRecordStruct;
+using kv_server::DeltaBasedRequestGenerator;
+using kv_server::FilePrefix;
+using kv_server::FileType;
+using kv_server::KeyValueMutationRecordStruct;
+using kv_server::KeyValueMutationType;
+using kv_server::KVFileMetadata;
+using kv_server::MessageQueue;
+using kv_server::MockBlobReader;
+using kv_server::MockBlobStorageChangeNotifier;
+using kv_server::MockBlobStorageClient;
+using kv_server::MockDeltaFileNotifier;
+using kv_server::MockDeltaFileRecordChangeNotifier;
+using kv_server::MockRealtimeNotifier;
+using kv_server::MockStreamRecordReader;
+using kv_server::MockStreamRecordReaderFactory;
+using kv_server::Record;
+using kv_server::ToDeltaFileName;
+using kv_server::ToFlatBufferBuilder;
+using kv_server::ToStringView;
+using kv_server::UserDefinedFunctionsConfigStruct;
+using kv_server::UserDefinedFunctionsLanguage;
+using kv_server::Value;
+using privacy_sandbox::server_common::MockMetricsRecorder;
+using testing::_;
+using testing::AllOf;
+using testing::ByMove;
+using testing::Field;
+using testing::Return;
+using testing::ReturnRef;
+
+namespace {
+
+BlobStorageClient::DataLocation GetTestLocation(
+    const std::string& basename = "") {
+  static constexpr absl::string_view kBucket = "testbucket";
+  return BlobStorageClient::DataLocation{.bucket = std::string(kBucket),
+                                         .key = basename};
+}
+
+absl::AnyInvocable<std::string(std::string_view)> GetRequestGenFn() {
+  return [](std::string_view key) {
+    return kv_server::CreateKVDSPRequestBodyInJson({std::string(key)});
+  };
+}
+
+class GenerateRequestsFromDeltaFilesTest : public ::testing::Test {
+ protected:
+  GenerateRequestsFromDeltaFilesTest()
+      : message_queue_(10000),
+        options_(DeltaBasedRequestGenerator::Options{
+            .data_bucket = GetTestLocation().bucket,
+            .message_queue = message_queue_,
+            .blob_client = blob_client_,
+            .delta_notifier = notifier_,
+            .change_notifier = change_notifier_,
+            .delta_stream_reader_factory = delta_stream_reader_factory_}) {}
+
+  MockBlobStorageClient blob_client_;
+  MockDeltaFileNotifier notifier_;
+  MockBlobStorageChangeNotifier change_notifier_;
+  MockStreamRecordReaderFactory delta_stream_reader_factory_;
+  MockMetricsRecorder metrics_recorder_;
+  MessageQueue message_queue_;
+  DeltaBasedRequestGenerator::Options options_;
+};
+
+TEST_F(GenerateRequestsFromDeltaFilesTest, LoadingDataFromDeltaFiles) {
+  ON_CALL(blob_client_, ListBlobs)
+      .WillByDefault(Return(std::vector<std::string>({})));
+  DeltaBasedRequestGenerator request_generator(
+      std::move(options_), std::move(GetRequestGenFn()), metrics_recorder_);
+  const std::string last_basename = "";
+  EXPECT_CALL(notifier_, Start(_, GetTestLocation(), last_basename, _))
+      .WillOnce([](BlobStorageChangeNotifier& change_notifier,
+                   BlobStorageClient::DataLocation location,
+                   std::string start_after,
+                   std::function<void(const std::string& key)> callback) {
+        callback(ToDeltaFileName(1).value());
+        LOG(INFO) << "Notified 1 file";
+        return absl::OkStatus();
+      });
+  EXPECT_CALL(notifier_, IsRunning).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(notifier_, Stop()).Times(1).WillOnce(Return(absl::OkStatus()));
+
+  absl::Notification all_records_loaded;
+  KVFileMetadata metadata;
+  auto update_reader = std::make_unique<MockStreamRecordReader>();
+  EXPECT_CALL(*update_reader, GetKVFileMetadata)
+      .Times(1)
+      .WillOnce(Return(metadata));
+  EXPECT_CALL(*update_reader, ReadStreamRecords)
+      .Times(1)
+      .WillOnce(
+          [](const std::function<absl::Status(std::string_view)>& callback) {
+            callback(
+                ToStringView(ToFlatBufferBuilder(DataRecordStruct{
+                    .record =
+                        KeyValueMutationRecordStruct{
+                            KeyValueMutationType::Update, 3, "key", "value"}})))
+                .IgnoreError();
+            return absl::OkStatus();
+          });
+  EXPECT_CALL(delta_stream_reader_factory_, CreateConcurrentReader)
+      .Times(1)
+      .WillOnce(Return(ByMove(std::move(update_reader))));
+  EXPECT_TRUE(request_generator.Start().ok());
+  EXPECT_TRUE(request_generator.IsRunning());
+  all_records_loaded.WaitForNotificationWithTimeout(absl::Seconds(2));
+  EXPECT_TRUE(request_generator.Stop().ok());
+  EXPECT_EQ(message_queue_.Size(), 1);
+  auto message_in_the_queue = message_queue_.Pop();
+  EXPECT_TRUE(message_in_the_queue.ok());
+  EXPECT_EQ(message_in_the_queue.value(),
+            kv_server::CreateKVDSPRequestBodyInJson({std::string("key")}));
+}
+
+}  // namespace

f25191bbf22b70a909eab0a142476f4d4d6777b8
Alexander Tretyakov <tretyakov@google.com>
2024-01-12 15:17:45 +0000

    feat: Add delta based realtime updates publisher
    Bug: 314190138
    Change-Id: I2802c8419f5f38d226d80dd8b89be7c0fff548d8
    GitOrigin-RevId: 3bc09736356b5eeb1f8229bc2a8c3a19a06bf6db

---
+++ tools/request_simulation/detla_based_realtime_updates_publisher.cc
@@ -0,0 +1,163 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tools/request_simulation/detla_based_realtime_updates_publisher.h"
+
+#include "absl/functional/bind_front.h"
+#include "public/data_loading/filename_utils.h"
+
+using privacy_sandbox::server_common::TraceWithStatusOr;
+
+namespace kv_server {
+namespace {
+// Holds an input stream pointing to a blob of Riegeli records.
+class BlobRecordStream : public RecordStream {
+ public:
+  explicit BlobRecordStream(std::unique_ptr<BlobReader> blob_reader)
+      : blob_reader_(std::move(blob_reader)) {}
+  std::istream& Stream() { return blob_reader_->Stream(); }
+
+ private:
+  std::unique_ptr<BlobReader> blob_reader_;
+};
+}  // namespace
+
+absl::Status DeltaBasedRealtimeUpdatesPublisher::Start() {
+  LOG(INFO) << "Start monitor delta files and publish them as realtime updates";
+  absl::Status status = options_.delta_notifier.Start(
+      options_.change_notifier, {.bucket = options_.data_bucket}, "",
+      absl::bind_front(
+          &DeltaBasedRealtimeUpdatesPublisher::EnqueueNewFilesToProcess, this));
+  if (!status.ok()) {
+    return status;
+  }
+  return data_load_thread_manager_->Start([this]() { ProcessNewFiles(); });
+}
+
+absl::Status DeltaBasedRealtimeUpdatesPublisher::Stop() {
+  {
+    absl::MutexLock l(&mu_);
+    stop_ = true;
+  }
+  LOG(INFO) << "Stopping publishing realtime updates from "
+            << options_.data_bucket;
+  if (options_.delta_notifier.IsRunning()) {
+    if (const auto status = options_.delta_notifier.Stop(); !status.ok()) {
+      LOG(ERROR) << "Failed to stop notify: " << status;
+    }
+  }
+  LOG(INFO) << "Delta notifier stopped";
+  LOG(INFO) << "Stopping publishing realtime updates from";
+  return data_load_thread_manager_->Stop();
+}
+
+bool DeltaBasedRealtimeUpdatesPublisher::IsRunning() const {
+  return data_load_thread_manager_->IsRunning();
+}
+
+void DeltaBasedRealtimeUpdatesPublisher::EnqueueNewFilesToProcess(
+    const std::string& basename) {
+  absl::MutexLock l(&mu_);
+  unprocessed_basenames_.push_front(basename);
+  LOG(INFO) << "queued " << basename << " for loading";
+}
+
+void DeltaBasedRealtimeUpdatesPublisher::ProcessNewFiles() {
+  LOG(INFO) << "Thread for new file processing started";
+  absl::Condition has_new_event(
+      this, &DeltaBasedRealtimeUpdatesPublisher::HasNewEventToProcess);
+  while (!data_load_thread_manager_->ShouldStop()) {
+    std::string basename;
+    {
+      absl::MutexLock l(&mu_, has_new_event);
+      if (stop_) {
+        LOG(INFO) << "Thread for new file processing stopped";
+        return;
+      }
+      basename = std::move(unprocessed_basenames_.back());
+      unprocessed_basenames_.pop_back();
+    }
+    LOG(INFO) << "Loading " << basename;
+    if (!IsDeltaFilename(basename)) {
+      LOG(WARNING) << "Received file with invalid name: " << basename;
+      continue;
+    }
+    RetryUntilOk(
+        [this, &basename] {
+          return TraceCreateRealtimeMessagesAndAddToQueue(
+              {.bucket = options_.data_bucket, .key = basename});
+        },
+        "LoadNewFileAndCreateRequest", [](const absl::Status&, int) {});
+  }
+}
+absl::StatusOr<DataLoadingStats>
+DeltaBasedRealtimeUpdatesPublisher::CreateRealtimeMessagesAndAddToQueue(
+    const BlobStorageClient::DataLocation& location) {
+  LOG(INFO) << "Loading " << location;
+  auto& blob_client = options_.blob_client;
+  auto record_reader =
+      options_.delta_stream_reader_factory.CreateConcurrentReader(
+          /*stream_factory=*/[&location, &blob_client]() {
+            return std::make_unique<BlobRecordStream>(
+                blob_client.GetBlobReader(location));
+          });
+  auto metadata = record_reader->GetKVFileMetadata();
+  if (!metadata.ok()) {
+    return metadata.status();
+  }
+  DataLoadingStats data_loading_stats;
+  const auto process_data_record_fn =
+      [this, &data_loading_stats](const DataRecord& data_record) {
+        if (data_record.record_type() == Record::KeyValueMutationRecord) {
+          KeyValueMutationRecordStruct kv_mutation_struct;
+          const auto* kv_mutation_record =
+              data_record.record_as_KeyValueMutationRecord();
+          if (kv_mutation_record->value_type() == Value::StringValue) {
+            const auto record =
+                GetTypedRecordStruct<KeyValueMutationRecordStruct>(data_record);
+            realtime_message_batcher_->Insert(std::move(record));
+            if (record.mutation_type == KeyValueMutationType::Update) {
+              data_loading_stats.total_updated_records++;
+            } else if (record.mutation_type == KeyValueMutationType::Delete) {
+              data_loading_stats.total_deleted_records++;
+            }
+            return absl::OkStatus();
+          }
+        }
+      };
+  auto status = record_reader->ReadStreamRecords(
+      [&process_data_record_fn](std::string_view raw) {
+        return DeserializeDataRecord(raw, process_data_record_fn);
+      });
+  if (!status.ok()) {
+    return status;
+  }
+  return data_loading_stats;
+}
+bool DeltaBasedRealtimeUpdatesPublisher::HasNewEventToProcess() const {
+  return !unprocessed_basenames_.empty() || stop_;
+}
+absl::StatusOr<DataLoadingStats>
+DeltaBasedRealtimeUpdatesPublisher::TraceCreateRealtimeMessagesAndAddToQueue(
+    BlobStorageClient::DataLocation location) {
+  return TraceWithStatusOr(
+      [location, this] {
+        return CreateRealtimeMessagesAndAddToQueue(location);
+      },
+      "CreateRealtimeMessagesAndAddToQueue",
+      {{"bucket", std::move(location.bucket)},
+       {"key", std::move(location.key)}});
+}
+
+}  // namespace kv_server

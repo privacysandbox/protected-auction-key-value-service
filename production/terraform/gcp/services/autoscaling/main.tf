c3b556328680acc61cc8bc5bcb255a944db95ac0
emmafu <emmafu@google.com>
2025-02-20 10:16:46 -0500

    chore: Add shard number to GCP instance name
    Bug: b/397921940
    Change-Id: I1aba0fa50002924af7196e9f8b82d59ab01d50f2
    GitOrigin-RevId: bee42e3210e8eb60e3cc127c6faba9eee3673358

--- production/terraform/gcp/services/autoscaling/main.tf
+++ production/terraform/gcp/services/autoscaling/main.tf
@@ -114,7 +114,7 @@ resource "google_compute_region_instance_group_manager" "kv_server" {
     }
   }
 
-  base_instance_name = "${var.service}-${var.environment}"
+  base_instance_name = "${var.service}-${var.environment}-${var.shard_num}"
 
   auto_healing_policies {
     health_check      = google_compute_health_check.kv_server.id
